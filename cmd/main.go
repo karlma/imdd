79c1180ab92c2a80122f99af5428c43a99f43d0e
karlma <karlcpp@gmail.com>
2023-07-17 20:04:36 +0800

    (IM-1) 重构了一下代码结构

--- cmd/main.go
+++ cmd/main.go
@@ -2,42 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"imd/internal/handler"
 	"net/http"
-	"os"
 )
 
 func main() {
 	fmt.Printf("start imd\n")
-	http.HandleFunc("/send", sendHandler)
+	http.HandleFunc("/send", handler.SendHandler)
 	http.ListenAndServe(":8080", nil)
-
-}
-
-func sendHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		fmt.Fprint(w, "The method is POST\n")
-		return
-	}
-	defer req.Body.Close()
-
-	b, _ := io.ReadAll(req.Body)
-
-	fn := "./data/msg.txt"
-	// TODO: 自动创建目录
-	f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		fmt.Fprintf(w, "Open file error: %v\n", err)
-		return
-	}
-
-	n, err := f.Write(append(b, '\n'))
-	if err != nil {
-		fmt.Fprintf(w, "Write file error: %v\n", err)
-		return
-	}
-	fmt.Printf("wrote %d bytes\n", n)
-	f.Sync()
-
-	fmt.Fprintf(w, "OK")
 }
---
+++ internal/handler/send_handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func SendHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		fmt.Fprint(w, "The method is POST\n")
+		return
+	}
+	defer req.Body.Close()
+
+	b, _ := io.ReadAll(req.Body)
+
+	fn := "./data/msg.txt"
+	// TODO: 自动创建目录
+	f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Fprintf(w, "Open file error: %v\n", err)
+		return
+	}
+
+	n, err := f.Write(append(b, '\n'))
+	if err != nil {
+		fmt.Fprintf(w, "Write file error: %v\n", err)
+		return
+	}
+	fmt.Printf("wrote %d bytes\n", n)
+	f.Sync()
+
+	fmt.Fprintf(w, "OK")
+}
