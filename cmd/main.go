2f7c2b32eaf8476cc7d0975e16dd29066c33a845
karlma <karlcpp@gmail.com>
2023-07-17 19:49:03 +0800

    (IM-1)实现一个webserver, 可以写文件了

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello imd\n")
+	fmt.Printf("start imd\n")
+	http.HandleFunc("/send", sendHandler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func sendHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		fmt.Fprint(w, "The method is POST\n")
+		return
+	}
+	defer req.Body.Close()
+
+	b, _ := io.ReadAll(req.Body)
+
+	f, err := os.Create("./data/msg.txt") // TODO: 自动创建目录的功能
+	if err != nil {
+		fmt.Fprintf(w, "Open file error: %v\n", err)
+		return
+	}
+
+	n, err := f.Write(b)
+	if err != nil {
+		fmt.Fprintf(w, "Write file error: %v\n", err)
+		return
+	}
+	fmt.Printf("wrote %d bytes\n", n)
+	f.Sync()
+
+	fmt.Fprintf(w, "OK")
 }
